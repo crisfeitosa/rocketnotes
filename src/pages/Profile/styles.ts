d139c14bee813f4f34cdd80c60a23f2113b6b4c6
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-22 18:05:20 -0300

    Avatar do usuário

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -24,4 +24,50 @@ export const Container = styled.div`
 export const Form = styled.form`
   max-width: 21rem;
   margin: 1.875rem auto 0;
+
+  > div:nth-child(4) {
+    margin-top: 1.5rem;
+  }
+`
+
+export const Avatar = styled.div`
+  position: relative;
+  margin: -7.5rem auto 2rem;
+
+  width: 11rem;
+  height: 11rem;
+  
+  > img {
+    border-radius: 50%;
+    width: 11rem;
+    height: 11rem;
+  }
+
+  > label {
+    width: 3rem;
+    height: 3rem;
+
+    background-color: ${({ theme }) => theme.COLORS.ORANGE};
+    border-radius: 50%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: absolute;
+    bottom: 0.5rem;
+    right: 0.5rem;
+
+    cursor: pointer;
+
+    input {
+      display: none;
+    }
+
+    svg {
+      width: 1.25rem;
+      height: 1.25rem;
+      color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+    }
+  }
 `
