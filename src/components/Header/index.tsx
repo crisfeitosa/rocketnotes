820eb150cd4fe60e8dd4365019ef400a2a1dc800
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-06-18 21:58:32 -0300

    Redirecionando o Logout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import { RiShutDownLine } from 'react-icons/ri';
+
 import { api } from '../../services/api';
 import { useAuth } from '../../hooks/useAuth';
-import { RiShutDownLine } from 'react-icons/ri';
 import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
 
 import { Container, Profile, Logout } from './styles';
 
 export function Header() {
+  const navigate = useNavigate();
   const { user, signOut } = useAuth();
+
   const avatarUrl = user?.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
+  function handleSignOut() {
+    navigate('/');
+    signOut();
+  }
 
   return (
     <Container>
@@ -23,7 +32,7 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
